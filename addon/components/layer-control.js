6e4d209e0e395f8b698052277dc8128540b602fc
Ashley Sommer <flubba86@gmail.com>
2016-10-31 16:23:55 +1000

    Update layer-control.js

--- addon/components/layer-control.js
+++ addon/components/layer-control.js
@@ -1,46 +1,46 @@
 import Ember from 'ember';
 import BaseLayer from 'ember-leaflet/components/base-layer';
-
 const {get,isEmpty,isBlank,isEqual} = Ember;
 
 export default BaseLayer.extend({
-  	layerSetup() {
-	    this._layer = this.createLayer();
-	    this._addObservers();
-	    this._addEventListeners();
-	    if (get(this,'containerLayer')) {
-	    	this._layer.addTo(get(this,'containerLayer')._layer);
-	    }
-	    this.didCreateLayer();
-  	},
+  	didInsertParent() {
+      this._layer = this.createLayer();
+      this._addObservers();
+      this._addEventListeners();
+      if (get(this,'parentComponent')) {
+        this._layer.addTo(get(this,'parentComponent')._layer);
+      }
+      this.didCreateLayer();
+    },
   	createLayer(){
   		return L.control.layers();
   	},
-  	didCreateLayer(){
-  		let layerGroups = get(this,'containerLayer._layerGroups');
+  	didCreateLayer() {
+      this._super(...arguments);
+  		var layerGroups = get(this,'parentComponent._layerGroups');
   		if (!isBlank(layerGroups)){
         layerGroups.forEach(layerGroup=>{
           if (layerGroup.default){
-            let containerLayer = get(this,'containerLayer._layer');
+            let containerLayer = get(this,'parentComponent._layer');
             layerGroup.layer.addTo(containerLayer);
           }
           this._layer.addOverlay(layerGroup.layer,layerGroup.name);
         });
       }
-      let baseLayers = get(this,'containerLayer._baseLayers');
+      let baseLayers = get(this,'parentComponent._baseLayers');
       if (!isBlank(baseLayers)){
         baseLayers.forEach(baseLayer=>{
           if (baseLayer.default){
-            let containerLayer = get(this,'containerLayer._layer');
+            let containerLayer = get(this,'parentComponent._layer');
             baseLayer.layer.addTo(containerLayer);
           }
           this._layer.addBaseLayer(baseLayer.layer,baseLayer.name);
         });
       }
-      
+
       let handler = get(this,'handler') || null;
       if (!isEmpty(handler)){
-        let map = get(this,'containerLayer')._layer;
+        let map = get(this,'parentComponent')._layer;
         map.on('overlayadd', this.attrs.handler);
         map.on('overlayremove', this.attrs.handler);
         map.on('baselayerchange', this.attrs.handler);
